401eb3258bd673cd5775f4aa7f5c7cf72ce79fd6
a33publicores <a33publicores@gmail.com>
2026-04-17 12:04:32 -0500

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
+const express = require("express")
+const cors = require("cors")
 
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.post("/login", (req,res)=>{
+
+const {usuario,clave} = req.body
+
+if(usuario === "admin" && clave === "1234"){
+return res.json({
+ok:true,
+usuario:"admin",
+rol:"admin"
+})
+}
+
+res.json({ok:false})
+
+})
+
+app.get("/",(req,res)=>{
+res.send("Backend activo 🚀")
+})
+
+app.listen(3000, ()=>{
+console.log("Servidor corriendo")
+})
